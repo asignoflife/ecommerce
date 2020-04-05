6f95634aa6d2e263a793a6854b7e07064bbd4758
Jae <asignoflife@gmail.com>
2020-04-04 22:09:46 -0700

    finishing sign in

--- src/App.js
+++ src/App.js
@@ -30,11 +30,11 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          });
+          })
         });
-      } else {
-        this.setState({ currentUser: userAuth });
-      }
+      } 
+        
+      this.setState({ currentUser: userAuth });
     });
   }
 
